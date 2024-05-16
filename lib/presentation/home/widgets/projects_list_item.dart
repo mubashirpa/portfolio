9e9633f50d0f228b1170a75e5a3c70c50922d5a3
Mubashir P A <mubashirpa2002@gmail.com>
2024-05-16 10:23:16 +0530

    Optimised navigation

--- lib/presentation/home/widgets/projects_list_item.dart
+++ lib/presentation/home/widgets/projects_list_item.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import '../../../navigation/screen_routes.dart';
 
 @immutable
-class ProjectsListItem extends StatefulWidget {
+class ProjectsListItem extends StatelessWidget {
   const ProjectsListItem({
     super.key,
     required this.title,
@@ -16,66 +14,48 @@ class ProjectsListItem extends StatefulWidget {
   final String imageUrl;
 
   @override
-  State<ProjectsListItem> createState() => _ProjectsListItemState();
-}
-
-class _ProjectsListItemState extends State<ProjectsListItem> {
-  bool isHovering = false;
-
-  @override
   Widget build(BuildContext context) {
     return AspectRatio(
-      aspectRatio: 1 / 1,
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 200),
-        curve: Curves.ease,
-        padding: EdgeInsets.all(isHovering ? 10 : 0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: InkWell(
-          onTap: () {
-            GoRouter.of(context).go(ScreenRoutes.ABOUT_SCREEN);
-          },
-          onHover: (hovering) {
-            setState(() => isHovering = hovering);
-          },
-          borderRadius: BorderRadius.circular(12),
-          child: Card(
-            margin: EdgeInsets.zero,
-            color: Theme.of(context).colorScheme.surfaceVariant,
-            child: Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    widget.title,
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  ),
-                  Text(
-                    widget.subTitle,
-                    style: Theme.of(context).textTheme.labelLarge,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Expanded(
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: Image.asset(
-                        widget.imageUrl,
-                        fit: BoxFit.fitHeight,
-                      ),
+      aspectRatio: 16 / 9,
+      child: InkWell(
+        onTap: () {
+          // TODO
+        },
+        borderRadius: BorderRadius.circular(12),
+        child: Card(
+          margin: EdgeInsets.zero,
+          color: Theme.of(context).colorScheme.surfaceVariant,
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  subTitle,
+                  style: Theme.of(context).textTheme.labelLarge,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Expanded(
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Image.asset(
+                      imageUrl,
+                      fit: BoxFit.fitHeight,
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
